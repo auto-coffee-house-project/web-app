2ccaa32e9df3f018b01628a845122e429d5bdffb
Eldos <eldos.baktybekov@gmail.com>
2024-01-24 12:37:53 +0600

    ✨ Add styles for client main page

--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -1,17 +1,24 @@
 <template>
-  <h3>Привет {{ userFirstName }}</h3>
-  <p>
-    Мы с удовольствием угостим бесплатным кофе, приходи к нам почаще и копи свои бонусы, ведь каждая {{ eachNthCup }}-я
-    кружка кофе в подарок!
-  </p>
-  <p>
-    Бесплатный напиток {{ nthCupProgressCurrent }}/{{ eachNthCup }}
-  </p>
-  <SaleTemporaryCodeContainer :bot="bot"/>
+  <ClientNavbar />
+  <main class="px-3 py-4">
+    <h3>Привет {{ userFirstName }}</h3>
+    <p>
+      Мы с удовольствием угостим бесплатным кофе, приходи к нам почаще и копи свои бонусы, ведь каждая {{ eachNthCup }}-я
+      кружка кофе в подарок!
+    </p>
+    <p>
+      Бесплатный напиток {{ nthCupProgressCurrent }}/{{ eachNthCup }}
+    </p>
+    <SaleTemporaryCodeContainer :bot="bot" />
+  </main>
 </template>
 
 <script setup>
+import ClientNavbar from '../components/ClientNavbar.vue';
 import SaleTemporaryCodeContainer from '../components/SaleTemporaryCodeContainer.vue';
+import { useTitle } from '@vueuse/core'
+
+useTitle('Клиент | Coffee House Bot')
 
 
 const props = defineProps({
