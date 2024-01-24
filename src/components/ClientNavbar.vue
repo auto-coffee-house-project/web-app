234d0dda998f1c22459119e80a53f709e3cd0a7c
Eldos <eldos.baktybekov@gmail.com>
2024-01-24 12:36:45 +0600

    ✨ Add client navbar

---
+++ src/components/ClientNavbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <ul class="nav nav-tabs">
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page">Подарок</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link">Каталог</a>
+    </li>
+  </ul>
+</template>
