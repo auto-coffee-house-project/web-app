1835711b1c1b88eab3f2c33edd8940dc6d18171e
Eldos <eldos.baktybekov@gmail.com>
2024-01-25 20:36:57 +0600

    :recycle: Move description into separate component

---
+++ src/components/Description.vue
@@ -0,0 +1,27 @@
+<template>
+  <h3>Привет {{ userFirstName }}</h3>
+  <p>
+    Мы с удовольствием угостим бесплатным кофе, приходи к нам почаще и копи свои бонусы, ведь каждая {{ eachNthCup }}-я
+    кружка кофе в подарок!
+  </p>
+  <p>
+    Бесплатный напиток {{ nthCupProgressCurrent }}/{{ eachNthCup }}
+  </p>
+</template>
+
+<script setup>
+const props = defineProps({
+  userFirstName: {
+    type: String,
+    required: true,
+  },
+  eachNthCup: {
+    type: Number,
+    required: true,
+  },
+  nthCupProgressCurrent: {
+    type: Number,
+    required: true,
+  },
+})
+</script>
--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -1,43 +1,25 @@
 <template>
-  <ClientNavbar />
+  <ClientNavbar/>
   <main class="px-3 py-4">
-    <h3>Привет {{ userFirstName }}</h3>
-    <p>
-      Мы с удовольствием угостим бесплатным кофе, приходи к нам почаще и копи свои бонусы, ведь каждая {{ eachNthCup }}-я
-      кружка кофе в подарок!
-    </p>
-    <p>
-      Бесплатный напиток {{ nthCupProgressCurrent }}/{{ eachNthCup }}
-    </p>
-    <SaleTemporaryCodeContainer :bot="bot" />
+    <Description
+      :nth-cup-progress-current="shop.nth_cup_progress_current"
+      :each-nth-cup="shop.each_nth_cup"
+      :user-first-name="user.first_name"
+    />
+    <SaleTemporaryCode :user="user" :bot="bot"/>
   </main>
 </template>
 
 <script setup>
 import ClientNavbar from '../components/ClientNavbar.vue';
-import SaleTemporaryCodeContainer from '../components/SaleTemporaryCodeContainer.vue';
 import { useTitle } from '@vueuse/core'
+import Description from "../components/Description.vue";
+import SaleTemporaryCode from "../components/SaleTemporaryCode.vue";
+import { inject } from "vue";
 
 useTitle('Клиент | Coffee House Bot')
 
-
-const props = defineProps({
-  bot: {
-    type: Object,
-    required: true,
-  },
-  userFirstName: {
-    type: String,
-    required: true,
-  },
-  eachNthCup: {
-    type: Number,
-    required: true,
-  },
-  nthCupProgressCurrent: {
-    type: Number,
-    required: true,
-  },
-})
-
+const user = inject('user')
+const bot = inject('bot')
+const shop = inject('shop')
 </script>
