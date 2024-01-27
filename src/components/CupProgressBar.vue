71a3589b3bbbf23a66c115c33b9f9e435e811dbb
Eldos <eldos.baktybekov@gmail.com>
2024-01-27 20:34:15 +0600

    :lipstick: Update UI

--- src/components/CupProgressBar.vue
+++ src/components/CupProgressBar.vue
@@ -2,7 +2,7 @@
   <div class="flex h-full w-full">
     <div
       class="h-6 bg-white border-black border first:rounded-l-md last:rounded-r-md grow"
-      :class="{'bg-sky-500': index <= currentValue}"
+      :class="{'bg-gray-400': index <= currentValue}"
       v-for="index in maxValue"
     >
     </div>
