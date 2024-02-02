e647ce5945811ea176311586033b154fc41e30c5
Eldos <eldos.baktybekov@gmail.com>
2024-02-02 10:09:40 +0600

    :fire: Remove QR code

--- src/components/SaleTemporaryCode.vue
+++ src/components/SaleTemporaryCode.vue
@@ -1,63 +1,50 @@
 <template>
-    <template v-if="code">
-        <p class="font-bold mt-4 mb-2 tg-text-primary">Скажите код: {{ code }}</p>
-        <p class="tg-text-primary">Ваш QR код:</p>
-        <VueQrcode
-                :value="qrCodeUrl"
-        />
-        <p class="tg-hint">Код обновится через: {{ codeRefreshesInSeconds }} сек.</p>
-    </template>
-    <LoadingSpinner v-else/>
+  <template v-if="code">
+    <p class="font-bold mt-4 mb-2 tg-text-primary">Скажите код: {{ code }}</p>
+    <p class="tg-hint">Код обновится через: {{ codeRefreshesInSeconds }} сек.</p>
+  </template>
+  <LoadingSpinner v-else/>
 </template>
 
 <script setup>
 import { useFetch, useIntervalFn } from "@vueuse/core";
-import VueQrcode from "@chenfengyuan/vue-qrcode";
-import { computed, onMounted, ref } from "vue";
+import { onMounted, ref } from "vue";
 import LoadingSpinner from "./LoadingSpinner.vue";
 
 
 const props = defineProps({
-    botId: {
-        type: Number,
-        required: true,
-    },
-    botUsername: {
-        type: String,
-        required: true,
-    },
-    userId: {
-        type: Number,
-        required: true,
-    },
+  botId: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type: Number,
+    required: true,
+  },
 })
 
 const code = ref()
 const codeRefreshesInSeconds = ref(30)
 
-const qrCodeUrl = computed(() => {
-    return `https://t.me/${props.botUsername}?start=scan-${code.value}`
-})
-
 const updateCode = async () => {
-    const url = `${import.meta.env.VITE_API_BASE_URL}/shops/codes/`
-    const {data} = await useFetch(url)
-        .post({
-            bot_id: props.botId,
-            client_user_id: props.userId,
-        })
-        .json()
-    code.value = data.value?.result?.code
+  const url = `${import.meta.env.VITE_API_BASE_URL}/shops/codes/`
+  const {data} = await useFetch(url)
+    .post({
+      bot_id: props.botId,
+      client_user_id: props.userId,
+    })
+    .json()
+  code.value = data.value?.result?.code
 }
 
 onMounted(updateCode)
 
 useIntervalFn(async () => {
-    codeRefreshesInSeconds.value -= 1
-    if (codeRefreshesInSeconds.value <= 0) {
-        await updateCode()
-        codeRefreshesInSeconds.value = 30
-    }
+  codeRefreshesInSeconds.value -= 1
+  if (codeRefreshesInSeconds.value <= 0) {
+    await updateCode()
+    codeRefreshesInSeconds.value = 30
+  }
 }, 1000)
 
 </script>
