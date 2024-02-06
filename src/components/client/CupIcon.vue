21fcf7be70888c39398e49131aea589189fea2ea
Eldos <eldos.baktybekov@gmail.com>
2024-02-06 12:57:38 +0600

    :sparkles: Add ability to display custom gift photo url

--- src/components/client/CupIcon.vue
+++ src/components/client/CupIcon.vue
@@ -2,7 +2,18 @@
   <img
     width="64"
     height="64"
-    src="https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/64/external-coffee-usa-flatart-icons-lineal-color-flatarticons.png"
+    :src="giftPhotoUrl || defaultPhotoUrl"
     alt="external-coffee-usa-flatart-icons-lineal-color-flatarticons"
   />
+
 </template>
+
+<script setup>
+defineProps({
+  giftPhotoUrl: {
+    type: String,
+  },
+})
+
+const defaultPhotoUrl = 'https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/64/external-coffee-usa-flatart-icons-lineal-color-flatarticons.png'
+</script>
