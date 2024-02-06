60f14dcfa872ad00004c1c4a339c223a22b59cd3
Eldos <eldos.baktybekov@gmail.com>
2024-02-06 12:58:38 +0600

    :children_crossing: Show code only after button clicked

--- src/views/client/ClientGiftView.vue
+++ src/views/client/ClientGiftView.vue
@@ -7,19 +7,30 @@
       :bot-id="botId"
     />
     <SaleTemporaryCode
+      v-if="isCodeSown"
       :bot-id="botId"
       :user-id="user.id"
+    />
+    <Button
+      class="w-full my-4"
+      label="Получить код"
+      v-if="!isCodeSown"
+      @click="isCodeSown = true"
     />
   </BasicContainer>
 </template>
 
 <script setup>
-import { inject } from 'vue';
+import { inject, ref } from 'vue';
 import { useTitle } from '@vueuse/core'
 import CupRequirementsInfo from '../../components/client/CupRequirementsInfo.vue'
 import ClientNavbar from '../../components/client/ClientNavbar.vue'
 import SaleTemporaryCode from '../../components/client/SaleTemporaryCode.vue'
 import BasicContainer from '../../layouts/BasicContainer.vue'
+
+import Button from 'primevue/button'
+
+const isCodeSown = ref(false)
 
 useTitle('Клиент | Coffee House Bot')
 
