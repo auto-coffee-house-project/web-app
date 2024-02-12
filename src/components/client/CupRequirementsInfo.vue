90a0afa396c3b0d3b7cab8bfeebccb7a90d538a7
Eldos <eldos.baktybekov@gmail.com>
2024-02-12 21:59:15 +0600

    :recycle: Use client statistics store instead of local store

--- src/components/client/CupRequirementsInfo.vue
+++ src/components/client/CupRequirementsInfo.vue
@@ -1,28 +1,25 @@
 <template>
-  <template v-if="userStatistics">
-    <CupDescription
-      :start-text="startText"
-      :user-first-name="userFirstName"
+  <CupDescription
+    :start-text="startText"
+    :user-first-name="userFirstName"
+  />
+  <div class="flex gap-x-3">
+    <CupIcon/>
+    <CupProgress
+      :current-cups-count="clientStatisticsStore.data?.current_cups_count"
+      :each-nth-cup-free="clientStatisticsStore.data?.each_nth_cup_free"
     />
-    <div class="flex gap-x-3">
-      <CupIcon/>
-      <CupProgress
-        :current-cups-count="userStatistics.current_cups_count"
-        :each-nth-cup-free="userStatistics.each_nth_cup_free"
-      />
-    </div>
-  </template>
-  <LoadingSpinner v-else/>
+  </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue'
 import { useIntervalFn } from '@vueuse/core'
-import LoadingSpinner from './LoadingSpinner.vue'
 import CupIcon from './CupIcon.vue'
 import CupProgress from './CupProgress.vue'
 import CupDescription from './CupDescription.vue'
-import { getBot, getClientStatistics } from '../../services/api.js'
+import { getBot } from '../../services/api.js'
+import useClientStatistics from '../../stores/useClientStatistics.js'
 
 const props = defineProps({
   botId: {
@@ -38,17 +35,19 @@ const props = defineProps({
     required: true,
   },
 })
-const userStatistics = ref()
 const startText = ref('')
 
+const clientStatisticsStore = useClientStatistics()
+
 const fetchUserStatistics = async () => {
-  const { data } = await getClientStatistics({ userId: props.userId, botId: props.botId })
-  userStatistics.value = data.value?.result
+  await clientStatisticsStore.fetchClientStatistics({ botId: props.botId, userId: props.userId })
 }
 
 onMounted(async () => {
-  await fetchUserStatistics()
-  const {data} = await getBot({botId: props.botId})
+  if (!clientStatisticsStore.data) {
+    await fetchUserStatistics()
+  }
+  const { data } = await getBot({ botId: props.botId })
   startText.value = data.value?.result?.start_text_client_web_app
 })
 
