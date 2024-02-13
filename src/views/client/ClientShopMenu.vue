87090c7623edd4b7184bb6343ea5620bb712035c
Eldos <eldos.baktybekov@gmail.com>
2024-02-13 23:02:25 +0600

    :bug: Fix client navbar items display on reload

--- src/views/client/ClientShopMenu.vue
+++ src/views/client/ClientShopMenu.vue
@@ -1,11 +1,15 @@
 <template>
   <ClientNavbar/>
-  menu
+  <BasicContainer>
+    <ShopMenuList/>
+  </BasicContainer>
 </template>
 
 <script setup>
 import ClientNavbar from '../../components/client/ClientNavbar.vue'
 import { useTitle } from '@vueuse/core'
+import BasicContainer from '../../layouts/BasicContainer.vue'
+import ShopMenuList from '../../components/client/ShopMenu/ShopMenuList.vue'
 
 useTitle('Каталог | Coffeconnect')
 </script>
