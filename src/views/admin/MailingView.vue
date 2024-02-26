484aa162e44d594c67dd3c429cc3e22aa8275133
Eldos <eldos.baktybekov@gmail.com>
2024-02-26 14:46:22 +0600

    :sparkles: Add ability to attach dynamic buttons

--- src/views/admin/MailingView.vue
+++ src/views/admin/MailingView.vue
@@ -20,64 +20,50 @@
         <Checkbox v-model="isKeyboardMarkup" binary input-id="isKeyboardMarkup"/>
         <label for="isKeyboardMarkup">Прикрепить Inline-клавиатуру</label>
       </div>
-      <div v-if="isKeyboardMarkup" class="flex flex-col gap-y-4">
-        <div class="flex flex-col">
-          <label for="keyboard-markup-text">Текст кнопки</label>
-          <InputText v-model="keyboardMarkupText" id="keyboard-markup-text" maxlength="64"/>
-        </div>
-        <div class="flex flex-col">
-          <label for="keyboard-markup-url">Ссылка кнопки</label>
-          <InputText v-model="keyboardMarkupUrl" id="keyboard-markup-url"/>
-        </div>
-      </div>
+      <KeyboardMarkupBuilder
+        v-if="isKeyboardMarkup"
+        v-model:buttons="buttons"
+      />
       <Button @click="onStartMailing" label="Начать" :disabled="isDisabled"/>
     </form>
   </BasicContainer>
 </template>
 
 <script setup>
-import AdminNavbar from '../../components/admin/AdminNavbar.vue'
+import { computed, ref } from 'vue'
 import Textarea from 'primevue/textarea'
-import { computed, ref, watchEffect } from 'vue'
-import BasicContainer from '../../layouts/BasicContainer.vue'
 import Checkbox from 'primevue/checkbox'
 import Button from 'primevue/button'
+import BasicContainer from '../../layouts/BasicContainer.vue'
 import MarkdownInfo from '../../components/admin/MarkdownInfo.vue'
-import InputText from 'primevue/inputtext'
-import { createMailing } from '../../services/api.js'
+import AdminNavbar from '../../components/admin/AdminNavbar.vue'
+import KeyboardMarkupBuilder from '../../components/admin/KeyboardMarkupBuilder.vue'
 import useBotStore from '../../stores/useBotStore.js'
+import { createMailing } from '../../services/api.js'
 
 const botStore = useBotStore()
 
 const text = ref('')
 const isMarkdown = ref(false)
-const keyboardMarkupText = ref('')
-const keyboardMarkupUrl = ref('')
-
-const keyboardMarkup = computed(() => {
-  let markup = []
-  if (keyboardMarkupText.value.length > 0 && keyboardMarkupUrl.value.length > 0) {
-    markup = [
-      {
-        text: keyboardMarkupText.value,
-        url: keyboardMarkupUrl.value,
-      }
-    ]
-  }
-  return markup
-})
-
 const isKeyboardMarkup = ref(false)
+
+const buttons = ref([])
 
 const isDisabled = computed(() => !text.value)
 
+const cleanedButtons = computed(() => {
+  return buttons
+    .value
+    .filter(({ text, url, isUrlValid }) => text && url && isUrlValid)
+    .map(({ text, url }) => ({ text, url }))
+})
+
 const onStartMailing = async () => {
-  console.log(keyboardMarkup.value)
   await createMailing({
     botId: botStore.id,
     text: text.value,
     isMarkdown: isMarkdown.value,
-    buttons: keyboardMarkup.value,
+    buttons: cleanedButtons.value,
   })
 }
 
