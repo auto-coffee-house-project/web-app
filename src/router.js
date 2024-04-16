fe950f14c7d6005a10f235d331abdadec42fe985
Eldos <eldos.baktybekov@gmail.com>
2024-04-16 11:26:26 +0600

    :recycle: Separate client and admin routes

--- src/router.js
+++ src/router.js
@@ -3,74 +3,64 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/admin',
-    children: [
-      {
-        path: '/clients/:id',
-        name: 'admin-client',
-        component: () => import('./views/admin/ClientDetailView.vue'),
-      },
-      {
-        path: '/clients',
-        name: 'admin-clients',
-        component: () => import('./views/admin/ClientListView.vue'),
-      },
-      {
-        path: '/mailing',
-        name: 'admin-mailing',
-        component: () => import('./views/admin/MailingView.vue'),
-      },
-      {
-        path: '/products/create',
-        name: 'admin-product-create',
-        component: () => import('./views/admin/ProductCreateFormView.vue'),
-      },
-      {
-        path: '/products/:id',
-        name: 'admin-product',
-        component: () => import('./views/admin/ProductView.vue'),
-      },
-      {
-        path: '/menu',
-        name: 'admin-shop-menu',
-        component: () => import('./views/admin/SettingsShopMenuView.vue'),
-      },
-      {
-        path: '/staff',
-        name: 'staff',
-        component: () => import('./views/admin/EmployeesView.vue'),
-      },
-      {
-        path: '/phrases',
-        name: 'phrases',
-        component: () => import('./views/admin/PhrasesView.vue'),
-      },
-      {
-        path: '/gift',
-        name: 'gift',
-        component: () => import('./views/admin/SettingsGiftView.vue'),
-      },
-      {
-        path: '/options',
-        name: 'options',
-        component: () => import('./views/admin/SettingsShopOptions.vue'),
-      },
-    ],
+    path: '/admin/clients/:id',
+    name: 'admin-client',
+    component: () => import('./views/admin/ClientDetailView.vue'),
   },
   {
-    path: '/client',
-    children: [
-      {
-        path: '/gift',
-        name: 'client-gift',
-        component: () => import('./views/client/ClientGiftView.vue'),
-      },
-      {
-        path: '/menu',
-        name: 'client-shop-menu',
-        component: () => import('./views/client/ClientShopMenu.vue'),
-      },
-    ],
+    path: '/admin/clients',
+    name: 'admin-clients',
+    component: () => import('./views/admin/ClientListView.vue'),
+  },
+  {
+    path: '/admin/mailing',
+    name: 'admin-mailing',
+    component: () => import('./views/admin/MailingView.vue'),
+  },
+  {
+    path: '/admin/products/create',
+    name: 'admin-product-create',
+    component: () => import('./views/admin/ProductCreateFormView.vue'),
+  },
+  {
+    path: '/admin/products/:id',
+    name: 'admin-product',
+    component: () => import('./views/admin/ProductView.vue'),
+  },
+  {
+    path: '/admin/menu',
+    name: 'admin-shop-menu',
+    component: () => import('./views/admin/SettingsShopMenuView.vue'),
+  },
+  {
+    path: '/staff',
+    name: 'staff',
+    component: () => import('./views/admin/EmployeesView.vue'),
+  },
+  {
+    path: '/phrases',
+    name: 'phrases',
+    component: () => import('./views/admin/PhrasesView.vue'),
+  },
+  {
+    path: '/gift',
+    name: 'gift',
+    component: () => import('./views/admin/SettingsGiftView.vue'),
+  },
+  {
+    path: '/options',
+    name: 'options',
+    component: () => import('./views/admin/SettingsShopOptions.vue'),
+  },
+  {
+    path: '/client/gift',
+    name: 'client-gift',
+    component: () => import('./views/client/ClientGiftView.vue'),
+  },
+  {
+    path: '/client/menu',
+    name: 'client-shop-menu',
+    component: () => import('./views/client/ClientShopMenu.vue'),
   },
   {
     path: '/unsupported',
